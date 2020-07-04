c7cd36d9f01390f1a7d1470ac04196a0cea5f096
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-07-04 15:32:56 -0300

    Finish App

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, Animated } from "react-native";
 
 export default function App() {
+  const [largura, setLargura] = useState(new Animated.Value(0));
+  const [altura, setAltura] = useState(new Animated.Value(30));
+
+  Animated.sequence([
+    Animated.timing(largura, {
+      toValue: 200,
+      duration: 2000,
+    }),
+    Animated.timing(altura, {
+      toValue: 350,
+      duration: 1000,
+    }),
+  ]).start();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Animated.View
+        style={{
+          width: largura,
+          height: altura,
+          backgroundColor: "#222",
+        }}
+      ></Animated.View>
     </View>
   );
 }
@@ -12,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
